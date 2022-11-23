8c17ff306418bfe9389450e9e291ee9c81b8ad65
chonkk <chonkk@hotmail.com>
2022-11-23 10:41:17 +0900

    Update readyevent 추가

--- src/main/java/com/tg360/webserver/WebserverApplication.java
+++ src/main/java/com/tg360/webserver/WebserverApplication.java
@@ -2,6 +2,8 @@ package com.tg360.webserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class WebserverApplication {
@@ -9,5 +11,9 @@ public class WebserverApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebserverApplication.class, args);
 	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void readyToStart(ApplicationReadyEvent event){System.out.println("Let's start :: http://localhost:8080");}
+
 
 }
